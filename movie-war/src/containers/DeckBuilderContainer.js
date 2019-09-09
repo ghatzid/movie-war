655bb5852637fc8ddc6898b7a87ee5d5d68866c0
Flatiron School <email.the.anderson@gmail.com>
2019-09-09 13:16:31 -0400

    first commit

---
+++ movie-war/src/containers/DeckBuilderContainer.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import MovieCard from '../components/MovieCard';
+
+class DeckBuilderContainer extends React.Component {
+
+    state = {
+        inDeck: [{ id: 'tt0033870', title: 'The Maltese Falcon', rating: 7 }, { id: "tt0120201", title: 'Starship Troopers', rating: 11 }, { id: "tt0120207", title: 'Steel', rating: 2 }, { id: "tt0120202", title: 'State and Main', rating: 4 }, { id: "tt0120200", title: 'Star Quest II', rating: 7.5 }, { id: 'tt0033870', title: 'The Maltese Falcon', rating: 7 }, { id: "tt0120201", title: 'Starship Troopers', rating: 11 }, { id: "tt0120207", title: 'Steel', rating: 2 }, { id: "tt0120202", title: 'State and Main', rating: 4 }, { id: "tt0120200", title: 'Star Quest II', rating: 7.5 }]
+    }
+
+    render() {
+        let deckComponents = this.state.inDeck.map(movie =>
+            <MovieCard
+                key={movie.tconst}
+                clickHandler = {this.props.clickHandler}
+                tconst={movie.tconst}
+                title={movie.title}
+                rating={movie.rating}
+            />
+        )
+        return (
+            <div>
+                <h1>Your deck</h1>
+                <div className="deck-builder-cards-container">
+                    {deckComponents}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default DeckBuilderContainer
