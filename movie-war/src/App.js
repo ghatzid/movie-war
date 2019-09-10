d65ff0a43f7076ee8c50f005f5a10769077ac126
Dave Bushnell <dybushnell@gmail.com>
2019-09-10 12:52:15 -0400

    create deck link

--- movie-war/src/App.js
+++ movie-war/src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import DeckContainer from './containers/DeckContainer'
-import GameContainer from './containers/GameContainer'
-import DeckBuilderContainer from './containers/DeckBuilderContainer'
-import WelcomeContainer from './containers/WelcomeContainer'
+import React from "react";
+import DeckContainer from "./containers/DeckContainer";
+import GameContainer from "./containers/GameContainer";
+import DeckBuilderContainer from "./containers/DeckBuilderContainer";
+import WelcomeContainer from "./containers/WelcomeContainer";
 
-import { Route, Redirect, BrowserRouter as Router } from 'react-router-dom'
+import { Route, BrowserRouter as Router } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
 class App extends React.Component {
   state = {
@@ -14,157 +14,177 @@ class App extends React.Component {
     comparatorHigher: true,
     playerDeck: [],
     AIDeck: [],
-    playerCard: {
-    },
-    AICard: {
-    }
-  }
+    playerCard: {},
+    AICard: {}
+  };
 
   componentDidMount() {
-    this.newGame()
+    this.newGame();
   }
 
   newGame = () => {
-    console.log('starting new game')
-    this.setState({playerCard:{} })
+    console.log("starting new game");
+    this.setState({ playerCard: {} });
     // if(this.state.playerDeck === []) {
-    fetch('http://localhost:3000/api/v1/cards/random/10')
-    .then(resp => resp.json())
-    .then(data => this.setState({playerDeck: data}, () => console.log("Player Deck:", this.state.playerDeck)))
+    fetch("http://localhost:3000/api/v1/cards/random/10")
+      .then(resp => resp.json())
+      .then(data =>
+        this.setState({ playerDeck: data }, () =>
+          console.log("Player Deck:", this.state.playerDeck)
+        )
+      );
     // .then(data => this.setState({playerDeck: data.slice(0,10), AIDeck: data.slice(12,22), AICard: data[Math.floor(Math.random()*data.length)]}, () =>     console.log("Player Deck:", this.state.playerDeck.length, "AIDeck:", this.state.AIDeck.length)
     // }
-    fetch('http://localhost:3000/api/v1/cards/random/10')
-    .then(resp => resp.json())
-    .then(data => this.setState({AIDeck: data}, () => this.newRound()))
-  }
-
-
+    fetch("http://localhost:3000/api/v1/cards/random/10")
+      .then(resp => resp.json())
+      .then(data => this.setState({ AIDeck: data }, () => this.newRound()));
+  };
 
   newRound = () => {
-    console.log('starting new round')
-    console.log("AI deck:", this.state.AIDeck)
+    console.log("starting new round");
+    console.log("AI deck:", this.state.AIDeck);
     this.setState({
       comparatorHigher: !this.state.comparatorHigher,
       playerCard: {},
-      AICard: this.state.AIDeck[Math.floor(Math.random() * this.state.AIDeck.length)]
-    })
-  }
+      AICard: this.state.AIDeck[
+        Math.floor(Math.random() * this.state.AIDeck.length)
+      ]
+    });
+  };
 
   checkWinCondition = () => {
     // console.log('checking win condition')
     // console.log("house deck cards:", this.state.AIDeck.length)
-    console.log("Player Deck:", this.state.playerDeck.length, "AIDeck:", this.state.AIDeck.length)
+    console.log(
+      "Player Deck:",
+      this.state.playerDeck.length,
+      "AIDeck:",
+      this.state.AIDeck.length
+    );
     if (this.state.playerDeck.length === 0) {
-      alert('Player loses, starting new game')
-      window.location.href = "/"
+      alert("Player loses, starting new game");
+      window.location.href = "/";
       // this.newGame()
-    }
-    else if (this.state.AIDeck.length === 0) {
+    } else if (this.state.AIDeck.length === 0) {
       // this.newGame()
-      window.location.href = "/"
-      alert('House loses, starting new game')
-
+      window.location.href = "/";
+      alert("House loses, starting new game");
+    } else {
+      this.newRound();
     }
-    else {
-      this.newRound()
-    }
-  }
+  };
 
-  commitHandler = (playerCard = this.state.playerCard, AICard = this.state.AICard) => {
+  commitHandler = (
+    playerCard = this.state.playerCard,
+    AICard = this.state.AICard
+  ) => {
     //if comparator is set to higher, victory condition is to possess higher rating
     if (this.state.comparatorHigher === true) {
       if (playerCard.rating > AICard.rating) {
-        let newArray = this.state.AIDeck.filter(movie => movie !== AICard)
-        this.setState({AIDeck: newArray}, () => this.checkWinCondition())
-        alert('player wins')
-      }
-      else if (playerCard.rating < AICard.rating) {
-        let newArray = this.state.playerDeck.filter(movie => movie.tconst !== playerCard.tconst)
-        this.setState({playerDeck: newArray}, () => this.checkWinCondition())
-        alert('player loses')
-      }
-      else {
-        alert('tie!')
-        this.checkWinCondition()
+        let newArray = this.state.AIDeck.filter(movie => movie !== AICard);
+        this.setState({ AIDeck: newArray }, () => this.checkWinCondition());
+        alert("player wins");
+      } else if (playerCard.rating < AICard.rating) {
+        let newArray = this.state.playerDeck.filter(
+          movie => movie.tconst !== playerCard.tconst
+        );
+        this.setState({ playerDeck: newArray }, () => this.checkWinCondition());
+        alert("player loses");
+      } else {
+        alert("tie!");
+        this.checkWinCondition();
       }
     }
     //if comparator is set to lower, victory condition is to possess lower rating
     else if (this.state.comparatorHigher === false) {
       if (playerCard.rating < AICard.rating) {
-        let newArray = this.state.AIDeck.filter(movie => movie.tconst !== AICard.tconst)
-        this.setState({AIDeck: newArray}, () => this.checkWinCondition())
-        alert('player wins')
-      }
-      else if (playerCard.rating > AICard.rating) {
-        let newArray = this.state.playerDeck.filter(movie => movie.tconst !== playerCard.tconst)
-        this.setState({playerDeck: newArray}, () => this.checkWinCondition())
-        alert('player loses')
-    
-      }
-      else {
-        alert('tie!')
-        this.checkWinCondition()
+        let newArray = this.state.AIDeck.filter(
+          movie => movie.tconst !== AICard.tconst
+        );
+        this.setState({ AIDeck: newArray }, () => this.checkWinCondition());
+        alert("player wins");
+      } else if (playerCard.rating > AICard.rating) {
+        let newArray = this.state.playerDeck.filter(
+          movie => movie.tconst !== playerCard.tconst
+        );
+        this.setState({ playerDeck: newArray }, () => this.checkWinCondition());
+        alert("player loses");
+      } else {
+        alert("tie!");
+        this.checkWinCondition();
       }
     }
-  }
+  };
 
-  slamHandler = (movie) => {
-    this.setState({ playerCard: movie })
-  }
+  slamHandler = movie => {
+    this.setState({ playerCard: movie });
+  };
 
-  newDeckHandler = (array) => {
-    if(array.length === 10) {
-      this.setState({playerDeck: array}, console.log("player deck set to:", array))
-      // return <Redirect to='/' />
+  newDeckHandler = array => {
+    if (array.length === 10) {
+      this.setState(
+        { playerDeck: array },
+        console.log("player deck set to:", array)
+      );
+    } else {
+      alert("not enough movies yet!");
     }
-    else {
-      alert("not enough movies yet!")
-    }
-  }
+  };
 
   render() {
     return (
       <Router>
-        <Route exact path="/" render={() => {
-          return (
-            <div className="app">
-              <div>
-                <WelcomeContainer />
+        <Route
+          exact
+          path="/"
+          render={() => {
+            return (
+              <div className="app">
+                <div>
+                  <WelcomeContainer />
+                </div>
               </div>
-
-            </div>)
-        }} />
-        <Route exact path="/game" render={() => {
-          return (
-            <div className="app">
-              <div>
-                <GameContainer
-                  clickHandler={this.commitHandler}
-                  cards={this.state}
-                  comparatorHigher={this.state.comparatorHigher}
-                />
+            );
+          }}
+        />
+        <Route
+          exact
+          path="/game"
+          render={() => {
+            return (
+              <div className="app">
+                <div>
+                  <GameContainer
+                    clickHandler={this.commitHandler}
+                    cards={this.state}
+                    comparatorHigher={this.state.comparatorHigher}
+                  />
+                </div>
+                <div>
+                  <DeckContainer
+                    clickHandler={this.slamHandler}
+                    deck={this.state.playerDeck}
+                    header="Your deck"
+                    subHeader={""}
+                  />
+                </div>
               </div>
-              <div>
-                <DeckContainer
-                  clickHandler={this.slamHandler}
-                  deck={this.state.playerDeck}
-                  header="Your deck"
-                />
+            );
+          }}
+        />
+        <Route
+          exact
+          path="/deckbuilder"
+          render={() => {
+            return (
+              <div className="app">
+                <div>
+                  <DeckBuilderContainer newDeckHandler={this.newDeckHandler} />
+                </div>
               </div>
-
-            </div>)
-        }} />
-        <Route exact path="/deckbuilder" render={() => {
-          return (
-            <div className="app">
-              <div>
-                <DeckBuilderContainer
-                  newDeckHandler = {this.newDeckHandler}
-                />
-              </div>
-            </div>
-          )
-        }} />
+            );
+          }}
+        />
       </Router>
     );
   }
--- movie-war/src/containers/DeckBuilderContainer.js
+++ movie-war/src/containers/DeckBuilderContainer.js
@@ -1,59 +1,81 @@
-import React from 'react'
-import DeckContainer from './DeckContainer'
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import DeckContainer from "./DeckContainer";
+import { Link } from "react-router-dom";
 
 class DeckBuilderContainer extends React.Component {
+  state = {
+    title: "",
+    potentialMovies: [],
+    addedMovies: [],
+    createDeckLink: ""
+  };
 
-    state = { title: "", potentialMovies: [], addedMovies: [] }
-
-    submitHandler = (e) => {
-        e.preventDefault()
-        this.setState({ title: "" })
-        fetch(`http://localhost:3000/api/v1/cards/search/${this.state.title}`)
-        .then(resp => resp.json())
-        .then(searchResults => this.setState({potentialMovies: searchResults}, () => console.log(this.state.potentialMovies)))
-    }
-
-    addHandler = (movie) => {
-        this.setState({addedMovies: [movie, ...this.state.addedMovies], potentialMovies: this.state.potentialMovies.filter(movie => !movie)})
-    }
-
-    changeHandler = (e) => {
-        this.setState({ [e.target.name]: e.target.value })
-    }
-
-
-    render() {
-
-        return (
-            <div>
-                <h1>Build your deck</h1>
-                <div className="game-container">
-                    Search for movie by title
-                    <form onSubmit={this.submitHandler}>
-                        <input type="text" placeholder="title" value={this.state.title} name="title" onChange={this.changeHandler} />
-                        <input type="submit" value="submit" />
-                    </form>
-                </div>
-                <DeckContainer 
-                    clickHandler = {this.addHandler}
-                    deck={this.state.potentialMovies}
-                    header={"Search results"}
-                />
-                <DeckContainer
-                    deck={this.state.addedMovies}
-                    header={'Your deck'}
-                />
-                    <Link className="submit-deck-button" onClick={() => this.props.newDeckHandler(this.state.addedMovies)} to={`/game`}><h4>Create Deck</h4></Link>
-
-                {/* <button onClick={() => this.props.newDeckHandler(this.state.addedMovies)}>Submit Deck</button> */}
-            </div>
+  submitHandler = e => {
+    e.preventDefault();
+    this.setState({ title: "" });
+    fetch(`http://localhost:3000/api/v1/cards/search/${this.state.title}`)
+      .then(resp => resp.json())
+      .then(searchResults =>
+        this.setState({ potentialMovies: searchResults }, () =>
+          console.log(this.state.potentialMovies)
         )
+      );
+  };
+
+  addHandler = movie => {
+    this.setState({
+      addedMovies: [movie, ...this.state.addedMovies],
+      potentialMovies: this.state.potentialMovies.filter(movie => !movie)
+    });
+    if (this.state.addedMovies.length === 9) {
+      this.setState({ createDeckLink: "Create deck" });
     }
+  };
 
+  changeHandler = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
+  render() {
+    return (
+      <div>
+        <h1>Build your deck</h1>
+        <div className="game-container">
+          Search for movie by title
+          <form onSubmit={this.submitHandler}>
+            <input
+              type="text"
+              placeholder="title"
+              value={this.state.title}
+              name="title"
+              onChange={this.changeHandler}
+            />
+            <input type="submit" value="submit" />
+          </form>
+        </div>
+        <DeckContainer
+          clickHandler={this.addHandler}
+          deck={this.state.potentialMovies}
+          header={"Search results"}
+          subHeader={""}
+        />
+        <DeckContainer
+          deck={this.state.addedMovies}
+          header={"Your deck"}
+          subHeader={this.state.addedMovies.length + "/10 movies"}
+        />
+        <Link
+          className="submit-deck-button"
+          onClick={() => this.props.newDeckHandler(this.state.addedMovies)}
+          to={"/game"}
+        >
+          <h4>{this.state.createDeckLink}</h4>
+        </Link>
 
+        {/* <button onClick={() => this.props.newDeckHandler(this.state.addedMovies)}>Submit Deck</button> */}
+      </div>
+    );
+  }
 }
 
-export default DeckBuilderContainer
+export default DeckBuilderContainer;
