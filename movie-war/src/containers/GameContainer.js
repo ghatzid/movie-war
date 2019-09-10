11cef07db1b3cf1cf4efa5a1cbeb5a975c161a9e
Dave Bushnell <dybushnell@gmail.com>
2019-09-10 16:00:15 -0400

    gameplay titles are back

--- movie-war/src/containers/GameContainer.js
+++ movie-war/src/containers/GameContainer.js
@@ -1,44 +1,43 @@
-import React from 'react'
-import PlayerCard from '../components/PlayerCard';
-import HouseCard from '../components/HouseCard';
+import React from "react";
+import PlayerCard from "../components/PlayerCard";
+import HouseCard from "../components/HouseCard";
 
 function GameContainer(props) {
-    // console.log('Game Container Props:', props)
-    let comparator; 
-    if(props.comparatorHigher === true) {
-        comparator = "You want to be higher than the house"
-    }
-    else {
-        comparator = "You want to be lower than the house"
-    }
-    return (
-        <div>
-            <h1>Your match</h1>
-            <div className="game-container">
-                <div className="house-card">
-                    <h3>House's Card</h3>
-                    <HouseCard
-                        tconst={props.cards.AICard.tconst}
-                        title={props.cards.AICard.title}
-                    />
-                </div>
-                <div className="comparator">
-                    <div className="comparator-text">
-                        <h1>{comparator}</h1>
-                    </div>
-                    <button onClick={() => props.clickHandler()}>Commit</button>
-                </div>
-                <div className="player-card">
-                    <h3>Player's Card</h3>
-                    <PlayerCard
-                        tconst={props.cards.playerCard.tconst}
-                        title={props.cards.playerCard.title}
-                        rating={props.cards.playerCard.rating}
-                    />
-                </div>
-            </div>
+  // console.log('Game Container Props:', props)
+  let comparator;
+  if (props.comparatorHigher === true) {
+    comparator = "You want to be higher than the house";
+  } else {
+    comparator = "You want to be lower than the house";
+  }
+  return (
+    <div>
+      <h1>Your match</h1>
+      <div className="game-container">
+        <div className="house-card">
+          <h3>House's Card</h3>
+          <HouseCard
+            tconst={props.cards.AICard.tconst}
+            title={props.cards.AICard.title}
+          />
         </div>
-    )
+        <div className="comparator">
+          <div className="comparator-text">
+            <h1>{comparator}</h1>
+          </div>
+          <button onClick={() => props.clickHandler()}>Commit</button>
+        </div>
+        <div className="player-card">
+          <h3>Player's Card</h3>
+          <PlayerCard
+            tconst={props.cards.playerCard.tconst}
+            title={props.cards.playerCard.title}
+            rating={props.cards.playerCard.rating}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default GameContainer
+export default GameContainer;
